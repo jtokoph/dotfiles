7860d068ca8ffa17fe721293b1a6c66b75b23167
Jason Tokoph <jason@tokoph.net>
2012-07-23 23:11:19 -0700

    Cleanup

---
+++ bash/aliases.bash
@@ -0,0 +1,35 @@
+# some from https://github.com/mathiasbynens/dotfiles/blob/master/.aliases
+
+# Files
+alias ll="ls -la"
+alias ..="cd .."
+alias ...="cd ../.."
+
+# Projects
+alias trunk="cd ~/Sites/mozes/trunk"
+alias labs="cd ~/Sites/mozes/labs"
+
+# SSH aliases
+alias ssh="ssh -A"
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en1"
+alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
+
+# Enhanced WHOIS lookups
+alias whois="whois -h whois-servers.net"
+
+# Flush Directory Service cache
+alias flush="dscacheutil -flushcache"
+
+# Trim new lines and copy to clipboard
+alias c="tr -d '\n' | pbcopy"
+
+# URL-encode strings
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
+
+# One of @janmoesen’s ProTip™s
+for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
+    alias "$method"="lwp-request -m '$method'"
+done
--- bash/config.bash
+++ bash/config.bash
@@ -1,6 +1,3 @@
-# Show git branch
-PS1='\[\033[1;33m\]\h\[\033[0;37m\]:\[\033[1;34m\]\W\[\033[1;32m\]$(__git_ps1 " (%s)")\[\033[1;31m\] · \[\033[0m\]'
-
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWSTASHSTATE=1
@@ -9,3 +6,30 @@ export GIT_PS1_SHOWSTASHSTATE=1
 export EDITOR="mate -w -l"
 
 export CLICOLOR=1
+
+# @gf3’s Sexy Bash Prompt, inspired by “Extravagant Zsh Prompt”
+# Shamelessly copied from https://github.com/gf3/dotfiles
+# Screenshot: http://i.imgur.com/s0Blh.png
+
+RED="\033[1;31m"
+ORANGE="\033[1;33m"
+GREEN="\033[1;32m"
+PURPLE="\033[1;35m"
+WHITE="\033[1;37m"
+BOLD=""
+RESET="\033[m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+GREEN="\033[1;32m"
+export RED
+export ORANGE
+export GREEN
+export PURPLE
+export WHITE
+export YELLOW
+export BLUE
+export BOLD
+export RESET
+
+export PS1="\[$YELLOW\]\h\[$WHITE\]:\[$BLUE\]\W\[$GREEN\]\$(__git_ps1 \" (%s)\")\[$RED\] · \[$RESET\]"
+export PS2="\[$ORANGE\]→ \[$RESET\]"
