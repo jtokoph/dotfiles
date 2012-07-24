b2c24e18944d062f275a8a52028a19e77d92850c
Jason Tokoph <jason@tokoph.net>
2012-07-24 11:18:44 -0700

    Change color

--- bash/config.bash
+++ bash/config.bash
@@ -12,7 +12,6 @@ export CLICOLOR=1
 # Screenshot: http://i.imgur.com/s0Blh.png
 
 RED="\033[1;31m"
-ORANGE="\033[1;33m"
 GREEN="\033[1;32m"
 PURPLE="\033[1;35m"
 WHITE="\033[1;37m"
@@ -22,7 +21,6 @@ YELLOW="\033[1;33m"
 BLUE="\033[1;34m"
 GREEN="\033[1;32m"
 export RED
-export ORANGE
 export GREEN
 export PURPLE
 export WHITE
@@ -32,4 +30,4 @@ export BOLD
 export RESET
 
 export PS1="\[$YELLOW\]\h\[$WHITE\]:\[$BLUE\]\W\[$GREEN\]\$(__git_ps1 \" (%s)\")\[$RED\] · \[$RESET\]"
-export PS2="\[$ORANGE\]→ \[$RESET\]"
+export PS2="\[$PURPLE\]→ \[$RESET\]"
