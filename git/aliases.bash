734476a3597dda8ba55132018b1ce1faec737770
Jason Tokoph <jason@tokoph.net>
2012-07-25 15:55:52 -0700

    add hub support to git

--- git/aliases.bash
+++ git/aliases.bash
@@ -1,3 +1,4 @@
+alias git='hub'
 alias g='git'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
