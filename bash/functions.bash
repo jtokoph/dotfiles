956dfd215113e5f10141356e48a276b52360f5a9
Jason Tokoph <jason@tokoph.net>
2012-07-27 12:43:32 -0700

    Fixes

--- bash/functions.bash
+++ bash/functions.bash
@@ -1,13 +1,13 @@
 export CUSTOM_TITLE_SET=0
 function title() {
-    echo -n -e "\033]1;$@\007";
+    echo -n -e "\033]0;$@\007";
     export CUSTOM_TITLE="$@";
     export CUSTOM_TITLE_SET=1;
 }
 
 function pc() {
     if [ $CUSTOM_TITLE_SET == 0 ]; then
-        echo -n -e "\033]1;`hostname | sed 's/\..*//'`\007";
+        echo -n -e "\033]0;`hostname | sed 's/\..*//'`\007";
     else
         title $CUSTOM_TITLE
     fi
