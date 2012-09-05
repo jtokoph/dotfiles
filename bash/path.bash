b326850d41d088541ec37f074d655b454734df39
Jason Tokoph <jason@tokoph.net>
2012-09-05 14:40:48 -0700

    remove old ruby path

--- bash/path.bash
+++ bash/path.bash
@@ -1,1 +1,1 @@
-export PATH=/usr/local/share/npm/bin:$HOME/.dotfiles/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:$HOME/.gem/ruby/1.8/bin:/usr/local/Cellar/ruby/1.9.3-p194/bin:$PATH
+export PATH=/usr/local/share/npm/bin:$HOME/.dotfiles/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/local/Cellar/ruby/1.9.3-p194/bin:$PATH
