7f09d60e36807c6bf6c9059c44876f905eda980d
Jason Tokoph <jason@tokoph.net>
2013-08-30 02:14:37 -0500

    fix path

--- bash/path.bash
+++ bash/path.bash
@@ -1,3 +1,3 @@
 export GOPATH=~/gocode
 export GOBIN=$GOPATH/bin
-export PATH=/usr/local/share/npm/bin:$HOME/.dotfiles/bin:$HOME/bin:/usr/local/sbin:$GOPATH/bin:$PATH
+export PATH=/usr/local/share/npm/bin:$HOME/.dotfiles/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:$GOPATH/bin:$PATH
