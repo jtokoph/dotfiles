34732b3adb86fbad19bdfec676e58dcfea001426
Jason Tokoph <jason@tokoph.net>
2013-09-24 22:31:57 -0700

    add python path

--- bash/path.bash
+++ bash/path.bash
@@ -1,3 +1,4 @@
 export GOPATH=~/gocode
 export GOBIN=$GOPATH/bin
 export PATH=/usr/local/share/npm/bin:$HOME/.dotfiles/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:$GOPATH/bin:$PATH
+export PATH=$PATH:/usr/local/share/python
