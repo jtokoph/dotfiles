d081daec3b0d4fa8f76837fa6dc51eec19cb6a40
Jason Tokoph <jason@tokoph.net>
2014-04-08 14:24:14 -0700

    Fix path and add irbrc file

--- bash/path.bash
+++ bash/path.bash
@@ -1,3 +1,13 @@
+### Added by the Heroku Toolbelt
+export PATH=$PATH:/usr/local/heroku/bin
+
+export GOPATH=~/gocode
 export GOPATH=~/gocode
 export GOBIN=$GOPATH/bin
-export PATH=/usr/local/share/npm/bin:$HOME/.dotfiles/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:$GOPATH/bin:$PATH
+export PATH=$GOPATH/bin:$PATH
+
+export PATH=/usr/local/share/npm/bin:$HOME/.dotfiles/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:$PATH
+export PATH=$PATH:/usr/local/share/python
+export PATH=./bin:$PATH
+
+export GOROOT=`go env GOROOT`
