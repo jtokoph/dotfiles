d3fa227ff9825aaf06e5e2b3ea5da2eab53cddcf
Jason Tokoph <jason@tokoph.net>
2014-07-17 14:00:08 -0700

    fix go paths

--- bash/path.bash
+++ bash/path.bash
@@ -1,10 +1,10 @@
 ### Added by the Heroku Toolbelt
 export PATH=$PATH:/usr/local/heroku/bin
 
-export GOPATH=~/gocode
-export GOPATH=~/gocode
-export GOBIN=$GOPATH/bin
-export PATH=$GOPATH/bin:$PATH
+export GOPATH=~/go
+export GOPATH=~/go
+#export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOPATH/bin
 
 export PATH=/usr/local/share/npm/bin:$HOME/.dotfiles/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:$PATH
 export PATH=$(brew --prefix ruby)/bin:$PATH
