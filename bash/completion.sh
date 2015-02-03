dd8a29ca84d41dbbcd8f9c37fa90fc832cb6d5d5
Jason Tokoph <jason@tokoph.net>
2015-02-02 21:47:30 -0800

    Add mosh completion and some git changes

--- bash/completion.sh
+++ bash/completion.sh
@@ -1,3 +1,10 @@
 if [ -f `brew --prefix`/etc/bash_completion ]; then
     . `brew --prefix`/etc/bash_completion
 fi
+
+_mosh ()
+{
+    COMPREPLY=()
+    _known_hosts_real -a -- "$(_get_cword)"
+} &&
+complete -F _mosh mosh
