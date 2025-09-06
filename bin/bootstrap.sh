9f89f6db9f6153ccb0c92588c03c2298b9e97ff4
Jason Tokoph <jason@tokoph.net>
2025-09-06 15:15:16 -0700

    Enable kanata

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+echo "Preparing sudo"
+sudo true
+
 # Install homebrew if not installed
 if ! command -v brew &> /dev/null; then
   echo "Installing homebrew"
@@ -26,6 +29,7 @@ BREW_PACKAGES=(
   stow
   font-hack-nerd-font
   zoxide
+  kanata
 )
 
 brew install $BREW_PACKAGES
@@ -36,3 +40,26 @@ stow -d ${HOME}/dotfiles -t ${HOME} .
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
 asdf install nodejs latest
+
+# Install Karabini-DriverKit-VirtualHIDDevice
+curl -L -o ${HOME}/Downloads/Karabiner-DriverKit-VirtualHIDDevice-6.1.0.pkg https://github.com/pqrs-org/Karabiner-DriverKit-VirtualHIDDevice/releases/download/v6.1.0/Karabiner-DriverKit-VirtualHIDDevice-6.1.0.pkg
+
+sudo installer -pkg ${HOME}/Downloads/Karabiner-DriverKit-VirtualHIDDevice-6.1.0.pkg -target /
+
+/Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager activate
+
+echo "Make sure to drag the kanata binary to Input Monitoring https://github.com/jtroo/kanata/issues/1211#issuecomment-2327141671"
+
+# Setup LaunchDaemons
+# sudo stow -d ${HOME}/dotfiles/LaunchDaemons -t /Library/LaunchDaemons .
+
+for file in ${HOME}/dotfiles/LaunchDaemons/*; do
+  echo "Setting up ${file}"
+  servicename=$(basename ${file} .plist)
+  filename="${servicename}.plist"
+  echo "Filename: ${filename}"
+  echo "Service name: ${servicename}"
+  sudo cp ${file} /Library/LaunchDaemons/
+  sudo launchctl load /Library/LaunchDaemons/${filename}
+  sudo launchctl start ${servicename}
+done
